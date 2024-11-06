fddf0e1697008710f3d0d51a60d015a8280f44bc
Chloe McKnight <cmcknight@bluebeam.com>
2024-11-06 10:15:13 -0600

    support multiple platforms for local dev workflow
    Signed-off-by: Chloe McKnight <cmcknight@bluebeam.com>

--- generate_bindings.sh
+++ generate_bindings.sh
@@ -3,7 +3,23 @@ set -euxo pipefail
 
 GEN_DIR="dotnet-tests/UniffiCS/gen"
 
+case $(uname | tr '[:upper:]' '[:lower:]') in
+	linux*)
+		LIB=libuniffi_fixtures.so
+	;;
+	darwin*)
+		LIB=libuniffi_fixtures.dylib
+	;;
+	msys* | mingw64*)
+		LIB=uniffi_fixtures.dll
+	;;
+	*)
+		echo "Cannot find binary - unrecognized uname" >&2
+		exit 1
+	;;
+esac
+
 rm -rf "$GEN_DIR"
 mkdir -p "$GEN_DIR"
 
-target/debug/uniffi-bindgen-cs target/debug/libuniffi_fixtures.so --library --out-dir="$GEN_DIR" --no-format
+target/debug/uniffi-bindgen-cs target/debug/$LIB --library --out-dir="$GEN_DIR" --no-format
